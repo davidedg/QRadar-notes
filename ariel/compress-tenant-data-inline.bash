c05ed9b5a2611f5ed8a67a3c9d03534036f0eeaf
Davide Del Grande <delgrande.davide@gmail.com>
2024-05-06 22:01:44 +0200

    Update compress-tenant-data-inline.bash

--- ariel/compress-tenant-data-inline.bash
+++ ariel/compress-tenant-data-inline.bash
@@ -4,12 +4,12 @@
 ##############################
 TENANT_ID="100000"
 GREP_INCLUDE='-e .'
-GREP_EXCLUDE='-v -e 2099/'
+GREP_EXCLUDE="-v -e $(date +'%Y/%-m/%-d')"
 ##############################
 TENANT_DIR=$(find /store/ariel/events/records/aux/$TENANT_ID/ -mindepth 0 -maxdepth 0 -type d -not -wholename "*//*")
 [[ -d $TENANT_DIR ]] || exit 5 # do not remove this check
 cd $TENANT_DIR || exit 5
-find . -mindepth 3 -maxdepth 3 -type d -not -wholename "*//*" | grep --color=never $GREP_INCLUDE | grep --color=never $GREP_EXCLUDE | while read -r datadir ; do
+ionice -t -c3 nice -n +19 find . -mindepth 3 -maxdepth 3 -type d -not -wholename "*//*" | grep --color=never $GREP_INCLUDE | grep --color=never $GREP_EXCLUDE | while read -r datadir ; do
   parentdir=$(dirname $datadir)
   daydir=$(basename $datadir)
   archive=$daydir.tar.gz
@@ -18,11 +18,20 @@ find . -mindepth 3 -maxdepth 3 -type d -not -wholename "*//*" | grep --color=nev
   cd $TENANT_DIR
   cd $parentdir || break
   
-  tar czf $archive $daydir
+  comment=""
+  if [[ ! -f $archive ]]; then
+    ionice -t -c3 nice -n +19 tar czf $archive $daydir
+  else
+    echo "# $archive exists - skipping"
+	comment="#"
+  fi
+  
   if [[ $? -eq 0 && -f $archive ]]; then
-	  echo "cd $TENANT_DIR && rm -rf $datadir"
+	  echo "$comment cd $TENANT_DIR && ionice -t -c3 nice -n +19 rm -rf $datadir"
   else
     echo " -- FAILED"
 	break
   fi
 done
+cd $TENANT_DIR
+##############################
